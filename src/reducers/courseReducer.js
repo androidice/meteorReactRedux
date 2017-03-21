7d5bd3a09995b9fc7d21cf63c0dcd424d7644640
Kevin Alviola <kevin@istackmanila.com>
2017-03-21 20:29:28 +0800

    reducers, fix

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,10 +1,9 @@
 export default function courseReducer(state = [], action){
   switch (action.type){
     case 'CREATE_COURSE':
-      state.push(action.course); // should not be done, because reducer should be a pure function, and
-      /// should not handle state mutations
-      return state;
-    break;
+      return [...state, // spread operator, explode or create new instance of state array
+             Object.assign({}, action.course)
+             ];
 
     default:
       return state;
