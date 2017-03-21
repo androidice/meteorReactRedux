b167ff2c9c07991750ec38b6c3852962b32906ee
Kevin Alviola <kevin@istackmanila.com>
2017-03-21 20:22:23 +0800

    reducers simple basics

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,11 @@
+export default function courseReducer(state = [], action){
+  switch (action.type){
+    case 'CREATE_COURSE':
+      state.push(action.course);
+      return state;
+    break;
+
+    default:
+      return state;
+  }
+}
