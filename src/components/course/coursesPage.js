3d17e3fac37dd04cfe61fb055c2533819cfab3aa
Kevin Alviola <kevin@istackmanila.com>
2017-03-21 12:46:42 +0800

    adding courses page

---
+++ src/components/course/coursesPage.js
@@ -0,0 +1,13 @@
+import React, {PropTypes} from 'react';
+
+export default class CoursesPage
+  extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
