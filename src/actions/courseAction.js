df4bfdf4d9639a206e702cd4e88b01a1afe1641b
Kevin Alviola <kevin@istackmanila.com>
2017-03-23 19:30:51 +0800

    added loadCourses thunk

--- src/actions/courseAction.js
+++ src/actions/courseAction.js
@@ -1,5 +1,20 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
+
 export function createCourse(course){
   return { type: types.CREATE_COURSE, course}; // the same with {course: course}
 }
 
+export function loadCoursesSucess(courses){
+  return {type: types.LOAD_COURSES_SUCCESS, courses}
+}
+
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi.getAllCourses().then((courses)=>{
+      dispatch(loadCoursesSucess(courses));
+    }).catch((error)=>{ //catch any exception
+      throw(error);
+    });
+  }
+}
