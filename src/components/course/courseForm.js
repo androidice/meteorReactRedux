ab0ee493d89b5c3ca4aba17cf9492d006ebcfeaa
Kevin Alviola <kevin@istackmanila.com>
2017-03-24 18:51:43 +0800

    adding CourseForm, with distructuring pattern

---
+++ src/components/course/courseForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors})=> {
+  return (
+    <form>
+      <h1>Manage Course</h1>
+    </form>
+  );
+};
+
+CourseForm.propTypes = {
+  course: React.PropTypes.object.isRequired,
+  allAuthors: React.PropTypes.array.isRequired,
+  onSave: React.PropTypes.func.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+  loading:  React.PropTypes.bool,
+  errors: React.PropTypes.object
+};
+
+export default CourseForm;
